5318226e52a6cfb11fa445ec2842e11742c5d5d4
EmanVR <157151199+EmanVR@users.noreply.github.com>
2024-01-26 15:13:55 -0600

    Update login.js

--- login.js
+++ login.js
@@ -4,8 +4,8 @@
 
         form.addEventListener('submit', function (event) {
             // Obtener los valores del formulario
-            const usuario = form.querySelector('[name="usuario"]').value.trim();
-            const contrasena = form.querySelector('[name="contrasena"]').value.trim();
+            const usuario = form.querySelector('[name="username"]').value.trim();
+            const contrasena = form.querySelector('[name="password"]').value.trim();
 
             // Validar que los campos no estén vacíos
             if (usuario === '' || contrasena === '') {
