12a124899d6074eb0898ec9587dcbd0984820b0d
EmanVR <157151199+EmanVR@users.noreply.github.com>
2024-01-26 19:56:37 -0600

    Update login.js

--- login.js
+++ login.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-
+    // Obtener el formulario y agregar un evento de escucha al envío
     const loginForm = document.querySelector('form');
     loginForm.addEventListener('submit', function (event) {
         event.preventDefault(); // Evitar que el formulario se envíe automáticamente
@@ -8,18 +8,30 @@ document.addEventListener('DOMContentLoaded', function () {
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
 
-        // Verificar si el correo electrónico es válido y la contraseña tiene al menos 8 caracteres
-        if (!validateEmail(email) || password.length < 8) {
-            alert('Por favor, ingrese un correo electrónico válido y una contraseña de al menos 8 caracteres.');
+        // Verificar si el correo electrónico es válido
+        if (!validateEmail(email)) {
+            alert('Por favor, ingrese un correo electrónico válido.');
         } else {
+            // Verificar la validez de la contraseña
+            if (validatePassword(password)) {
 
-            window.location.href = 'Principal.html';
+                window.location.href = 'Principal.html';
+            } else {
+                alert('La contraseña debe tener una mayúscula, un número, y tener entre 8 y 30 caracteres.');
+            }
         }
     });
 
+    // Función para validar el formato del correo electrónico
     function validateEmail(email) {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return emailRegex.test(email);
     }
-});
 
+    // Función para validar la contraseña
+    function validatePassword(password) {
+
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d).{8,30}$/;
+        return passwordRegex.test(password);
+    }
+});
