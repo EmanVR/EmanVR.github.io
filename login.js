600ef74f874a8b23992b1b60683e923d506e4bb4
EmanVR <157151199+EmanVR@users.noreply.github.com>
2024-01-26 20:08:01 -0600

    Update login.js

--- login.js
+++ login.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Obtener el formulario y agregar un evento de escucha al envío
+
     const loginForm = document.querySelector('form');
     loginForm.addEventListener('submit', function (event) {
-        event.preventDefault(); // Evitar que el formulario se envíe automáticamente
+        event.preventDefault();
 
-        // Obtener los valores de correo electrónico y contraseña
+
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
 
-        // Verificar si el correo electrónico es válido
+
         if (!validateEmail(email)) {
             alert('Por favor, ingrese un correo electrónico válido.');
         } else {
-            // Verificar la validez de la contraseña
+  
             if (validatePassword(password)) {
 
                 window.location.href = 'Principal.html';
@@ -22,13 +22,13 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // Función para validar el formato del correo electrónico
+
     function validateEmail(email) {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
         return emailRegex.test(email);
     }
 
-    // Función para validar la contraseña
+
     function validatePassword(password) {
 
         const passwordRegex = /^(?=.*[A-Z])(?=.*\d).{8,30}$/;
