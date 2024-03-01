b3a5fc8ddd83705f6180a40778ecad6d7ff2ab3a
EmanVR <157151199+EmanVR@users.noreply.github.com>
2024-03-01 17:19:00 -0600

    Create register.js

---
+++ register.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const registerForm = document.querySelector('form');
+    registerForm.addEventListener('submit', function (event) {
+        event.preventDefault(); // TOMAR EN CUENTA ESTA LÍNEA
+
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        if (!validateEmail(email)) {
+            alert('Por favor, ingrese un correo electrónico válido.');
+        } else {
+            if (validatePassword(password)) {
+                // Aquí iría el código para registrar al usuario en la base de datos
+                alert('Registro exitoso.');
+            } else {
+                alert('La contraseña debe tener una mayúscula, un número, y tener entre 8 y 30 caracteres.');
+            }
+        }
+    });
+
+    function validateEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+
+    function validatePassword(password) {
+        const passwordRegex = /^(?=.*[A-Z])(?=.*\d).{8,30}$/;
+        return passwordRegex.test(password);
+    }
+});
